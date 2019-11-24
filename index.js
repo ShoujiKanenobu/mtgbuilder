8a7cf86130a014ad00e9b823062a74313581e08a
ShoujiKanenobu <shoujikanenobu@gmail.com>
2019-11-23 18:24:03 -0800

    Added some cool stuff

--- index.js
+++ index.js
@@ -1,85 +1,16 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
+const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
+const mtg = require('mtgsdk')
+const handler = require('./deckHandler')
+const bodyParser = require("body-parser")
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .post('/getRate', (req, res) => {
-    let ren = calculateRate(req);
-    res.render('pages/getRate', ren)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  function calculateRate(req) {
-    let mailType = req.query.mailType;
-    let weight = parseInt(req.query.weight);
-    let rate = 0;
-
-    switch(mailType)
-    {
-      case 'LettersStamped':
-          if(weight > 1)
-          {
-              rate = 0.55;
-              weight--;
-          }
-          while(weight > 0)
-          {
-            rate += 0.15;
-            weight--;
-          }
-
-      break;
-      case 'LettersMetered':
-          if(weight > 1)
-          {
-              rate = 0.5;
-              weight--;
-          }
-          while(weight > 0)
-          {
-            rate += 0.15;
-            weight--;
-          }
-      break;
-      case 'LargeEnvelopes':
-        if(weight > 1)
-          {
-              rate = 1;
-              weight--;
-          }
-          while(weight > 0)
-          {
-            rate += 0.15;
-            weight--;
-          }
-      break;
-      case 'FirstClass':
-          if(weight < 5) {
-            rate = 3.66;
-          } else if (weight < 9) {
-            rate = 4.39;
-          } else if (weight < 13) {
-            rate = 5.19;
-          } else {
-            rate = 5.71;
-          }
-      break;
-    }
-    return { mailType: mailType, weight: weight, rate: rate };
-  }
-
-  showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-      for (i = 0; i < times; i++) {
-        result += i + ' '
-      }
-  return result;
-  }
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.post('/saveDeck', handler.handleList)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
